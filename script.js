8a75909a491e213a36b68de579e93225715fdcb0
findyoumed <postnews@daum.net>
2026-03-30 11:15:40 +0900

    Update

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 
 document.addEventListener('DOMContentLoaded', () => {
     initScrollAnimations();
-    initStatsCounter();
     initMobileMenu();
     initSmoothScrolling();
     initScrollTop();
